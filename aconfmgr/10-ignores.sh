7aff1186c1b1d448a64948bb72e067430e512151
Jakob Schnitzer <mail@jakobschnitzer.de>
2024-05-02 20:26:36 +0200

    restore some necessary /var/lib ignores

--- aconfmgr/10-ignores.sh
+++ aconfmgr/10-ignores.sh
@@ -36,8 +36,8 @@ if [[ "$machine_type" == "server" ]]; then
     IgnorePath '/var/lib/letsencrypt'
 fi
 
-IgnorePath '/etc/bluetooth'
-IgnorePath '/etc/brlapi.key'
+# IgnorePath '/etc/bluetooth'
+# IgnorePath '/etc/brlapi.key'
 IgnorePath '/etc/conf.d/lm_sensors'
 IgnorePath '/etc/colord'                    # auto-generated
 IgnorePath '/etc/cups/subscriptions.conf'   # auto-generated
@@ -62,8 +62,7 @@ IgnorePath '/etc/ssl/certs/*'                         # extracted ssl certificat
 IgnorePath '/etc/ssh/*_key'                           # secret
 IgnorePath '/etc/ssh/*_key.pub'                       # secret
 IgnorePath '/etc/NetworkManager/system-connections/*' # secret
-IgnorePath '/etc/pacman.d/gnupg/*'
-IgnorePath '/etc/lvm/*'
+IgnorePath '/etc/pacman.d/gnupg/*'                    # pacman key stuff
 
 IgnorePath '/etc/udev/hwdb.bin'     # large auto-generated file
 IgnorePath '/usr/lib/udev/hwdb.bin' # large auto-generated file
@@ -71,7 +70,7 @@ IgnorePath '/usr/lib/udev/hwdb.bin' # large auto-generated file
 IgnorePath '/usr/share/info/dir'
 IgnorePath '/usr/share/mime/*'  # Ignore mime type config
 IgnorePath '/usr/lib/modules/*' # linux kernel modules stuff
-IgnorePath '/usr/lib/utempter/*'
+IgnorePath '/usr/lib/utempter/utempter'
 
 # Ignore stuff in var lib - this should only ignore stuff for installed packages.
 IgnorePath '/var/.updated' # systemd-generated file
@@ -94,6 +93,18 @@ IgnorePath '/var/lib/passim/*'
 IgnorePath '/var/lib/systemd/*'
 IgnorePath '/var/lib/tlp/*'
 IgnorePath '/var/lib/upower/*'
+
+# Empty dirs that are automatically created somehow
+IgnorePath '/var/lib/BrlAPI'
+IgnorePath '/var/lib/brltty'
+IgnorePath '/var/lib/geoclue'
+IgnorePath '/var/lib/gnome-remote-desktop'
+IgnorePath '/var/lib/lastlog'
+IgnorePath '/var/lib/libuuid'
+IgnorePath '/var/lib/machines'
+IgnorePath '/var/lib/portables'
+IgnorePath '/var/lib/private'
+IgnorePath '/var/lib/tpm2-tss/system/keystore'
 
 IgnorePath '/var/db/sudo/lectured' # users that were shown the sudo intro lecture message
 IgnorePath '/var/log/*'            # Ignore logs
