d86108c4382767194952aa4de8b5fc1a7dfcbd7e
Jakob Schnitzer <mail@jakobschnitzer.de>
2024-07-21 18:49:55 +0200

    nvim: adjust for which-key update; use neovim comments

--- nvim_init.lua
+++ nvim_init.lua
@@ -167,7 +167,6 @@ vim.cmd.colorscheme("gruvbox")
 -- colorizer colors CSS colors
 require("colorizer").setup()
 require("mini.align").setup()
-require("mini.comment").setup()
 require("mini.surround").setup()
 require("mini.statusline").setup({ use_icons = false })
 
@@ -175,19 +174,17 @@ require("todo-comments").setup()
 
 vim.opt.timeout = true
 vim.opt.timeoutlen = 300
-require("which-key").setup()
-require("which-key").register({
-  ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-  ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-  ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-  ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-  ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-  ["<leader>t"] = { name = "[T]est", _ = "which_key_ignore" },
-  ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+require("which-key").setup({ icons = { rules = false } })
+require("which-key").add({
+  { "<leader>c", group = "[C]ode" },
+  { "<leader>d", group = "[D]ocument" },
+  { "<leader>r", group = "[R]ename" },
+  { "<leader>s", group = "[S]earch" },
+  { "<leader>w", group = "[W]orkspace" },
+  { "<leader>t", group = "[T]est" },
+  { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 })
-require("which-key").register({
-  ["<leader>h"] = { "Git [H]unk" },
-}, { mode = "v" })
+
 -- }}}
 -- gitsigns {{{
 -- keybindings taken from
